9bf83dab0a9d8cd8f794a27c6bc99b58e6fe6fb2
KaiAliton <keks.com.lyl@gmail.com>
2023-03-30 14:37:32 +0500

    git init

---
+++ apps/auth/serializers/register.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from apps.user.serializers import UserSerializer
+from apps.user.models import User
+
+
+class RegisterSerializer(UserSerializer):
+    password = serializers.CharField(max_length=128,
+                                     min_length=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email',
+                  'username', 'first_name', 'last_name',
+                  'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
